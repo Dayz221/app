f50409ce0bfbd425c0a13446166cc2966d6c2bfc
Dayz221 <rutuchenal37@gmail.com>
2024-01-31 19:30:37 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import { useEffect, useState } from "react";
 import './appg.css'
 import classNames from "classnames";
+import Timer from "./timer.js";
 
 const App = () => {
-  const [days, setDays] = useState(0)
-  const [hours, setHours] = useState(0)
-  const [minutes, setMinutes] = useState(0)
-  const [seconds, setSeconds] = useState(0)
-  const deadline = new Date(2024, 1, 7, 0, 0, 0, 0).getTime()
-  const [isCritical, setIsCritical] = useState(false)
-
-  useEffect(() => {
-    setInterval(() => {
-      const date = new Date()
-      const delta = deadline - date.getTime()
-
-      if (delta >= 0) {
-        const a = Math.floor(delta / (1000*60*60*24))
-        const a_ = delta % (1000*60*60*24)
-        const b = Math.floor(a_ / (1000*60*60))
-        const b_ = a_ % (1000*60*60)
-        const c = Math.floor(b_ / (1000*60))
-        const c_ = b_ % (1000*60)
-        const d = Math.floor(c_ / 1000)
-
-        setDays(a)
-        setHours(b)
-        setMinutes(c)
-        setSeconds(d)
-
-        setIsCritical(delta < 1000*60*60*24)
-      }
-    }, 100)
-  }, [])
+  
 
   return (
-    <div className={classNames("app", {critical: isCritical})}>
-      <h1 className="title">
-        До сдачи пояснительных записок осталось:
-      </h1>
-      <h2 className="date">
-        {String(days).padStart(2, '0')}:
-        {String(hours).padStart(2, '0')}:
-        {String(minutes).padStart(2, '0')}:
-        {String(seconds).padStart(2, '0')}
-      </h2>
+    <div className="app">
+      <Timer title="До сдачи пояснительных записок:" deadline={new Date(2024, 1, 7, 0, 0, 0, 0).getTime()} />
+      <Timer title="До региона осталось:" deadline={new Date(2024, 1, 20, 0, 0, 0, 0).getTime()} />
     </div>
   )
 }
---
+++ src/timer.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react"
+import classNames from "classnames"
+
+const Timer = ({deadline, title}) => {
+    const [days, setDays] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+    const [isCritical, setIsCritical] = useState(false)
+
+    useEffect(() => {
+        setInterval(() => {
+        const date = new Date()
+        const delta = deadline - date.getTime()
+
+        if (delta >= 0) {
+            const a = Math.floor(delta / (1000*60*60*24))
+            const a_ = delta % (1000*60*60*24)
+            const b = Math.floor(a_ / (1000*60*60))
+            const b_ = a_ % (1000*60*60)
+            const c = Math.floor(b_ / (1000*60))
+            const c_ = b_ % (1000*60)
+            const d = Math.floor(c_ / 1000)
+
+            setDays(a)
+            setHours(b)
+            setMinutes(c)
+            setSeconds(d)
+
+            setIsCritical(delta < 1000*60*60*24)
+        } else {
+            setDays(0)
+            setHours(0)
+            setMinutes(0)
+            setSeconds(0)
+
+            setIsCritical(delta < 1000*60*60*24)
+        }
+        }, 100)
+    }, [])
+    
+    return (
+        <div className={classNames("timer", {critical: isCritical})}>
+            <h1 className="title">
+                {title}
+            </h1>
+            <h2 className="date">
+                {String(days).padStart(2, '0')}:
+                {String(hours).padStart(2, '0')}:
+                {String(minutes).padStart(2, '0')}:
+                {String(seconds).padStart(2, '0')}
+            </h2>
+        </div>
+    )
+}
+
+export default Timer
