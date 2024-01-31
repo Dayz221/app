ea6330bf7bd955946a1e5302f14557f47d201e32
Dayz221 <rutuchenal37@gmail.com>
2024-01-31 19:03:17 +0300

    beta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './app.css'
 
-function App() {
+const App = () => {
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const deadline = new Date(2024, 1, 7, 0, 0, 0, 0).getTime()
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date()
+      const delta = deadline - date.getTime()
+      console.log(delta)
+      if (delta >= 0) {
+        const a = Math.floor(delta / (1000*60*60*24))
+        const a_ = delta % (1000*60*60*24)
+        const b = Math.floor(a_ / (1000*60*60))
+        const b_ = a_ % (1000*60*60)
+        const c = Math.floor(b_ / (1000*60))
+        const c_ = b_ % (1000*60)
+        const d = Math.floor(c_ / 1000)
+
+        setDays(a)
+        setHours(b)
+        setMinutes(c)
+        setSeconds(d)
+      }
+    }, 100)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title">
+        До сдачи пояснительных записок осталось:
+      </h1>
+      <h2 className="date">
+        {String(days).padStart(2, '0')}:
+        {String(hours).padStart(2, '0')}:
+        {String(minutes).padStart(2, '0')}:
+        {String(seconds).padStart(2, '0')}
+      </h2>
     </div>
-  );
+  )
 }
 
 export default App;
