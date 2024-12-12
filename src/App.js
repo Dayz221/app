6dfe02e95e0e50be4c915de27b49c7d5d2471427
Dayz221 <rutuchenal37@gmail.com>
2024-12-12 22:02:49 +0300

    avokado

--- src/App.js
+++ src/App.js
@@ -22,11 +22,12 @@ const App = () => {
 
   return (
     <div className="app">
-      <div className="title2">Часы судного дня</div>
+      <div className="title2">Ты отчислен из-за ПТП. Держи авокадо!</div>
       {/* <Timer title={!memMode ? "ПЗ сдаем на регионе:" : "До пиз***ей от Алексеича (Палыча) за ПЗ:"} deadline={new Date(2024, 1, 20, 0, 0, 0, 0).getTime()} />
       <Timer title={!memMode ? "До начала региона осталось:" : "До пиз***ей от Алексеича (Палыча) перед регионом:"}deadline={new Date(2024, 1, 20, 0, 0, 0, 0).getTime()} />
       <Timer title={!memMode ? "До конца региона осталось:" : "До пиз***ей от Алексеича (Палыча) после региона:"}deadline={new Date(2024, 1, 22, 0, 0, 0, 0).getTime()} /> */}
-      <Timer title={!memMode ? "До отчисления по ПТП осталось:" : "До пиз***ей от Алексеича (Палыча) после региона:"}deadline={new Date(2024, 11, 12, 22, 0, 0, 0).getTime()} />
+      {/* <Timer title={!memMode ? "До отчисления по ПТП осталось:" : "До пиз***ей от Алексеича (Палыча) после региона:"}deadline={new Date(2024, 11, 12, 22, 0, 0, 0).getTime()} /> */}
+      <img style={{maxWidth: "640px", width: "100%", margin: "10px"}} src="https://goodkitchen.su/image/catalog/05005/avokado-polovinki-poperechnoe-sechenie-2115922.jpg" />
       {/* <div className="memBlock">
         <button type="button" className={classNames('memButton', {active: memMode})} onClick={() => setMem()}>Реальное отображение*</button>
       </div> */}
